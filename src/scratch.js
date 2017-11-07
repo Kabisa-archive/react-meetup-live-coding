9f833d69d672b0996a917fb7eb857e93134f245e
Matthijs Groen <matthijs.groen@gmail.com>
2017-11-07 21:09:47 +0100

    Static content rendering

--- src/scratch.js
+++ src/scratch.js
@@ -1,6 +1,22 @@
 import "babel-polyfill";
+import { h, render } from "preact";
 
-// insert code here ...
+const root = document.querySelector("#maji-app");
+root.innerHTML = "";
+render(
+  <section>
+    <h1>Hello there</h1>
+    <p>This is a html page</p>
+    <p>With some paragraphs</p>
+    <ul>
+      <li>Todo 1</li>
+      <li>Todo 2</li>
+      <li>Todo 3</li>
+    </ul>
+  </section>,
+  root,
+  root.firstChildElement
+);
 
 if (process.env.NODE_ENV !== "production") {
   require("preact/devtools");
