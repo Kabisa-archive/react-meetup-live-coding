4d2df6a9b9efa62038870824875d2f59421d485e
Matthijs Groen <matthijs.groen@gmail.com>
2017-11-08 20:29:06 +0100

    Extracted into components

--- src/scratch.js
+++ src/scratch.js
@@ -1,18 +1,27 @@
 import "babel-polyfill";
 import { h, render } from "preact";
 
+const TodoItem = ({ name }) => <li>{name}</li>;
+
+const TodoList = ({ items }) => (
+  <ul>{items.map((item, i) => <TodoItem name={item.name} />)}</ul>
+);
+
 const root = document.querySelector("#maji-app");
 root.innerHTML = "";
+
+const todoItems = [
+  { name: "Todo 1", done: true },
+  { name: "Todo 2", done: false },
+  { name: "Todo 3", done: false }
+];
+
 render(
   <section>
     <h1>Hello there</h1>
     <p>This is a html page</p>
     <p>With some paragraphs</p>
-    <ul>
-      <li>Todo 1</li>
-      <li>Todo 2</li>
-      <li>Todo 3</li>
-    </ul>
+    <TodoList items={todoItems} />
   </section>,
   root,
   root.firstChildElement
